be37099ed528434896638d5b7631622e74ca8ac4
crossi72 <claudio@eurosoftlab.com>
2022-02-10 10:01:40 +0100

    release 1.0.0
    Co-authored-by: Emanuele <Mnovanta@users.noreply.github.com>
    Co-authored-by: Alex-Franklyn <Alex-Franklyn@users.noreply.github.com>
    Co-authored-by: Michele <michelerispoli@users.noreply.github.com>
    Co-authored-by: RitaGabrieli <RitaGabrieli@users.noreply.github.com>

--- recupero/js/script.js
+++ recupero/js/script.js
@@ -1,16 +1,55 @@
 //lego le funzioni alla pressione dei pulsanti
 //alla pressione del pulsante con ID "rosso" chiama la funzione aggiungiRosso
 document.getElementById("rosso").addEventListener("click", aggiungi_rosso);
+document.getElementById("verde").addEventListener("click", aggiungi_verde);
+document.getElementById("blu").addEventListener("click", aggiungi_blu);
 
 //funzioni: ogni funzione aggiunge una classe a tutti i div che hanno classe "quadrato"
 
-//funzione aggiungiRosso
 //cerco tutti gli elementi con classe "quadrato"
 //ad ogni elemento trovato aggiungo la classe "rosso"
 function aggiungi_rosso() {
 	var mieiDiv = document.getElementsByClassName("quadrato");
 
+	//rimuovo le classi già presenti
+	rimuovi_classi(mieiDiv);
+
 	for (i = 0; i < mieiDiv.length; i++) {
 		mieiDiv[i].classList.add("rosso");
+	}
+}
+
+function rimuovi_classi(mieiDiv) {
+	for (i = 0; i < mieiDiv.length; i++) {
+		mieiDiv[i].classList.remove("bianco");
+		mieiDiv[i].classList.remove("rosso");
+		mieiDiv[i].classList.remove("verde");
+		mieiDiv[i].classList.remove("blu");
+	}
+}
+
+//cerco tutti gli elementi con classe "quadrato"
+//ad ogni elemento trovato aggiungo la classe "verde"
+function aggiungi_verde() {
+	var mieiDiv = document.getElementsByClassName("quadrato");
+
+	//rimuovo le classi già presenti
+	rimuovi_classi(mieiDiv);
+
+	for (i = 0; i < mieiDiv.length; i++) {
+		mieiDiv[i].classList.add("verde");
+	}
+}
+
+//cerco tutti gli elementi con classe "quadrato"
+//ad ogni elemento trovato aggiungo la classe "blu"
+function aggiungi_blu() {
+	var mieiDiv = document.getElementsByClassName("quadrato");
+
+	//rimuovo le classi già presenti
+	rimuovi_classi(mieiDiv);
+
+	for (i = 0; i < mieiDiv.length; i++) {
+		mieiDiv[i].classList.add("blu");
 	}
 }
